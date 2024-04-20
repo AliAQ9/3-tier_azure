1e832fb487f823b980027acc0467c16324cd2fdb
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-20 16:33:38 -0500

    cleanup

--- resources/vnet/subnet.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  count                = length(var.subnet_cidrs)
-  name                 = "subnet${count.index}"
-  resource_group_name  = azurerm_resource_group.azure_project.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_cidrs[count.index]]
-}
