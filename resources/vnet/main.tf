e811b486dbf7f0a429e14c55e62a36d5cebae1b8
Olubukola Ajayi <152934516+bukieajayi@users.noreply.github.com>
2024-04-20 19:26:56 +0000

    updated subnet in main.ft

--- resources/vnet/main.tf
+++ resources/vnet/main.tf
@@ -54,3 +54,11 @@ location            = "Central US"
 resource_group_name = azurerm_resource_group.azure_project.name
  allocation_method   = "Static"
 }
+
+resource "azurerm_subnet" "subnet" {
+  count                = length(var.subnet_cidrs)
+  name                 = "subnet${count.index}"
+  resource_group_name  = azurerm_resource_group.azure_project.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_cidrs[count.index]]
+}
