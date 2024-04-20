3b7dfbc25304d0e030f6e93d55e2a686d65ab6b2
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-20 12:58:53 -0500

    update

--- resources/variables.tf
+++ resources/variables.tf
@@ -2,10 +2,11 @@ variable "subnet_cidrs" {
   type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
-
 variable "location" {
-
     type = string
-    default = US-East
- 
+    default = "US-East"
+}
+variable "azurerm_resource_group.resource  "name" {
+    type = string
+    default = "azure_project"
 }
