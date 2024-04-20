a3942495817557b87f0df88ea05871663c47e465
Olubukola Ajayi <152934516+bukieajayi@users.noreply.github.com>
2024-04-20 19:28:32 +0000

    updated traffic mng into main.tf

--- resources/vnet/main.tf
+++ resources/vnet/main.tf
@@ -62,3 +62,40 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_cidrs[count.index]]
 }
+
+resource "random_id" "server" {
+  keepers = {
+    azi_id = 1
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "azure_project" {
+  name     = "traffic_manager"
+  location = "West Europe"
+}
+
+resource "azurerm_traffic_manager_profile" "traffic_manager" {
+  name                   = random_id.server.hex
+  resource_group_name    = azurerm_resource_group.azure_project
+  traffic_routing_method = "Weighted"
+
+  dns_config {
+    relative_name = random_id.server.hex
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol                     = "HTTP"
+    port                         = 80
+    path                         = "/"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 9
+    tolerated_number_of_failures = 3
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
