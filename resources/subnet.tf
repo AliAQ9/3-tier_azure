2215d700e55bb964513bf51457a4a9f3bf73b11b
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-20 10:07:26 -0500

    updated subnets

--- resources/subnet.tf
+++ resources/subnet.tf
@@ -3,21 +3,21 @@ resource "azurerm_resource_group" "azure_project" {
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "sub1" {
   name                = "example-vnet"
   address_space       = ["10.0.1.0/16"]
   location            = azurerm_resource_group.azure_project
   resource_group_name = azurerm_resource_group.azure_project
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "sub2" {
   name                = "example-vnet"
   address_space       = ["10.0.2.0/16"]
   location            = azurerm_resource_group.azure_project
   resource_group_name = azurerm_resource_group.azure_project
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "sub3" {
   name                = "example-vnet"
   address_space       = ["10.0.3.0/16"]
   location            = azurerm_resource_group.azure_project
