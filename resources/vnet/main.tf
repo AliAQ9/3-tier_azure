c5c9a8bccb3b3dc38a6a1d16f449bc4b9fc024d7
Olubukola Ajayi <152934516+bukieajayi@users.noreply.github.com>
2024-04-20 19:24:59 +0000

    updated scale set into main.tf

--- resources/vnet/main.tf
+++ resources/vnet/main.tf
@@ -42,3 +42,15 @@ resource "azurerm_key_vault" "project_keyvault" {
     ]
   }
 }
+ resource "azurerm_lb" "TestLoadBalancer" {
+ name                = "TestLoadBalancer"
+ location            = "Central US"
+ resource_group_name = azurerm_resource_group.azure_project.name
+}
+
+resource "azurerm_public_ip" "PublicIP" {
+ name                = "PublicIP"
+location            = "Central US"
+resource_group_name = azurerm_resource_group.azure_project.name
+ allocation_method   = "Static"
+}
