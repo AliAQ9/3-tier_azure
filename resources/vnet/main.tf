4146bd61daf7965289cc331086a63c5d179f40c5
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-20 16:49:23 -0500

    added vnet to main

--- resources/vnet/main.tf
+++ resources/vnet/main.tf
@@ -14,6 +14,13 @@ resource "azurerm_resource_group" "azurerm_key_vault" {
   location = "East US"
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  location            = azurerm_resource_group.azure_project.location
+  resource_group_name = azurerm_resource_group.azure_project.name
+  address_space       = ["10.0.0.0/16"]
+}
+
 resource "azurerm_key_vault" "project_keyvault" {
   name                        = "examplekeyvault"
   location                    = azurerm_resource_group.azure_project
