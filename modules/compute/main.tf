7e2ed7080f59c6bb4f6b602d20f05a9db35a31dd
valentinabalan <159856621+valentinabalan@users.noreply.github.com>
2024-04-20 22:17:06 -0500

    add main

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -120,3 +120,30 @@ resource "azurerm_public_ip" "pip" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm"
+  resource_group_name = azurerm_resource_group.azure_project.name
+  location            = azurerm_resource_group.azure_project.location
+  size                = "Standard_B1ls"
+  admin_username      = "valentinabalan"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+ 
+  admin_ssh_key {
+    username   = "valentinabalan"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
