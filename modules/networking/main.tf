217859333b0cd6179ba89a87257a4e9dfc296975
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-21 11:49:55 -0500

    added secg to vnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -11,6 +11,17 @@ resource "azurerm_network_security_group" "vnet-secg" {
   name                = "vnet-secg"
   location            = var.location
   resource_group_name = var.name
+   security_rule {
+    name                       = "vnet-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
