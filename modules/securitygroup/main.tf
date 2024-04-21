46a88b9fadb3918a43ba477a535586abc054c3a4
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-21 09:24:52 -0500

    updated secg

--- modules/securitygroup/main.tf
+++ modules/securitygroup/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "azure-task" {
   location =    var.location
 }
 
-resource "azurerm_network_security_group" "ssh_secg" {
-  name                = "ssh_secg"
+resource "azurerm_network_security_group" "ssh-secg" {
+  name                = "ssh-secg"
   location            = var.location
   resource_group_name = var.name
 
@@ -21,24 +21,55 @@ resource "azurerm_network_security_group" "ssh_secg" {
   }
 }
 
-resource "azurerm_resource_group" "azure-task" {
-  name     =    var.name
-  location =    var.location
-}
-
-resource "azurerm_network_security_group" "http_secg" {
-  name                = "http_secg"
+resource "azurerm_network_security_group" "http-secg" {
+  name                = "http-secg"
   location            = var.location
   resource_group_name = var.name
 
   security_rule {
     name                       = "HTTP"
-    priority                   = 100
+    priority                   = 101
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "80"
     destination_port_range     = "80"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "0.0.0.0/0"
+  }
+}
+
+resource "azurerm_network_security_group" "https-secg" {
+  name                = "https-secg"
+  location            = var.location
+  resource_group_name = var.name
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "443"
+    destination_port_range     = "443"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "0.0.0.0/0"
+  }
+}
+
+resource "azurerm_network_security_group" "mysql-secg" {
+  name                = "mysql-secg"
+  location            = var.location
+  resource_group_name = var.name
+
+  security_rule {
+    name                       = "MYSQL"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "3306"
+    destination_port_range     = "3306"
     source_address_prefix      = "0.0.0.0/0"
     destination_address_prefix = "0.0.0.0/0"
   }
