ed702c1d536befb699c615ef50c12a82aafbb7eb
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-20 20:15:00 -0500

    updated main

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+module "resourcegroup" {
+  source         = "./modules/resourcegroup"
+  name           = var.name
+  location       = var.location
+}
+
