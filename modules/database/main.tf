05bf3dca904fbc42895a698b36c1824faa7611f0
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-23 18:32:52 -0500

    updated db

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -9,11 +9,11 @@ resource "azurerm_resource_group" "azure_project" {
 
 resource "azurerm_mysql_server" "mysqlserver" {
   name                = var.server_name
-  location            = var.name
+  location            = var.location
   resource_group_name = var.name
 
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
+  administrator_login          = var.primary_database_admin
+  administrator_login_password = var.primary_database_password
 
   sku_name   = "GP_Gen5_2"
   storage_mb = 5120
@@ -40,126 +40,3 @@ resource "azurerm_mysql_database" "db" {
     prevent_destroy = true
   }
 }
-
-##################### code needs fix ###################
-/*
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "${var.prefix}-SN"
-  resource_group_name  = var.name
-  virtual_network_name = var.virtual_network_name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-resource "azurerm_public_ip" "vm" {
-  name                = "${var.prefix}-PIP"
-  location            = var.location
-  resource_group_name = var.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "${var.prefix}-NSG"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_network_security_rule" "RDPRule" {
-  name                        = "RDPRule"
-  resource_group_name         = var.name
-  priority                    = 1000
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = 3389
-  source_address_prefix       = "167.220.255.0/25"
-  destination_address_prefix  = "*"
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-
-resource "azurerm_network_security_group" "mysql-secg" {
-  name                = "mysql-secg"
-  location            = var.location
-  resource_group_name = var.name
-
-  security_rule {
-    name                       = "MYSQL"
-    priority                   = 104
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "3306"
-    destination_port_range     = "3306"
-    source_address_prefix      = "0.0.0.0/0"
-    destination_address_prefix = "0.0.0.0/0"
-  }
-}
-resource "azurerm_network_interface" "example" {
-  name                = "${var.prefix}-NIC"
-  location            = var.location
-  resource_group_name = var.name
-
-  ip_configuration {
-    name                          = "exampleconfiguration1"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm.id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "${var.prefix}-VM"
-  location              = var.location
-  resource_group_name   = var.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS14_v2"
-
-  storage_image_reference {
-    publisher = "MicrosoftSQLServer"
-    offer     = "SQL2017-WS2016"
-    sku       = "SQLDEV"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.prefix}-OSDisk"
-    caching           = "ReadOnly"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  os_profile {
-    computer_name  = "winhost01"
-    admin_username = "exampleadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_windows_config {
-    timezone                  = "Pacific Standard Time"
-    provision_vm_agent        = true
-    enable_automatic_upgrades = true
-  }
-}
-
-resource "azurerm_mssql_virtual_machine" "example" {
-  virtual_machine_id = azurerm_virtual_machine.example.id
-  sql_license_type   = "PAYG"
-}
-*/
--- modules/database/variable.tf
+++ modules/database/variable.tf
@@ -2,3 +2,7 @@ variable "name" {}
 variable "location" {}
 variable "server_name" {}
 variable "virtual_network_name" {}
+variable "dbsubnetname" {}
+variable "dbsubnetcidr" {}
+variable "primary_database_admin" {}
+variable "primary_database_password" {}
