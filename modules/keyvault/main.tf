cdf7ffb8db6d2410dfab38e27da2c532d6e50aa9
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-24 15:50:59 -0500

    bug fix

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,12 +1,3 @@
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
-
 resource "azurerm_resource_group" "azure-task" {
   name     = var.name
   location = var.location
