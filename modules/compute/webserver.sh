8f7c8c4b169c95aa29dca4a9839de13f35d0e6f5
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-23 21:57:34 -0500

    updated scripts

--- modules/compute/webserver.sh
+++ modules/compute/webserver.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd
+
 sudo yum install http -y
+sudo systemctl start http
+sudo systemctl enable http
+sudo systemctl status http
--- modules/database/dbserver.sh
+++ modules/database/dbserver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo yum install mysql -y
+sudo systemctl start mysql
+sudo systemctl enable mysql
+sudo systemctl status mysql
