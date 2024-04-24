b1539d5d7fab51efe98d2b61abe389dceb346a44
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-24 15:52:49 -0500

    tweak

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
 resource "azurerm_resource_group" "azure-task" {
   name     = var.name
   location = var.location
