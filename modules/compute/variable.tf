6091e0d36d8d7fd9f96da8a1ec919d4924f677ca
Ali Abdul-Qadir <qep5319@gmail.com>
2024-04-24 13:19:25 -0500

    fix

--- modules/compute/variable.tf
+++ modules/compute/variable.tf
@@ -3,12 +3,12 @@ variable "location" {}
 variable "vnet_name" {}
 variable "security_group_name" {}
 variable "websubnetname" {}
-variable "appsubnetname" {}
-variable "appsubnetcidr" {}
 variable "websubnetcidr" {}
 variable "web_host_name" {}
 variable "web_username" {}
 variable "web_os_password" {}
+variable "appsubnetname" {}
+variable "appsubnetcidr" {}
 variable "app_host_name"{}
 variable "app_username" {}
 variable "app_os_password" {}
